20493ab2023f6f3d5776f5ab1c566b7bf8b57324
Ahmed EL-KADHI <ael-kadh@e3r2p3.42.fr>
2016-05-13 20:19:25 +0200

    2 done

--- Colleen.c
+++ Colleen.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-//ahmed
+/*
+	ahmed
+*/
 
 void vide() {
-//fonction vide
+/*
+	fonction vide
+*/
 }
 
 int main() {
 char n = 10;
-char *a = "#include <stdio.h>%c%c//ahmed%c%cvoid vide() {%c//fonction vide%c}%c%cint main() {%cchar n = 10;%cchar *a = %c%s%c;%c";
-char *b = "char *b = %c%s%c;%cprintf(a, n, n, n, n, n, n, n, n, n, n, 34, a, 34, n);%cprintf(b, 34, b, 34, n, n, n, n, n);%creturn 0;%c}%c";
-printf(a, n, n, n, n, n, n, n, n, n, n, 34, a, 34, n);
+char *a = "#include <stdio.h>%c%c/*%c%cahmed%c*/%c%cvoid vide() {%c/*%c%cfonction vide%c*/%c}%c%cint main() {%cchar n = 10;%cchar *a = %c%s%c;%c";
+char *b = "char *b = %c%s%c;%cprintf(a, n, n, 10, 9, 10, n, n, n, 10, 9, 10, n, n, n, n, n, 34, a, 34, n);%cprintf(b, 34, b, 34, n, n, n, n, n);%creturn 0;%c}%c";
+printf(a, n, n, 10, 9, 10, n, n, n, 10, 9, 10, n, n, n, n, n, 34, a, 34, n);
 printf(b, 34, b, 34, n, n, n, n, n);
 return 0;
 }
---
+++ Grace.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/*
+	ahmed
+*/
+#define anfang(fp) char *a="#include <stdio.h>%c/*%c%cahmed%c*/%c#define anfang(fp) char *a=%c%s%c;fprintf(fp, a, 10, 10, 9, 10, 10, 34, a, 34);";fprintf(fp, a, 10, 10, 9, 10, 10, 34, a, 34);
+#define mittle(fp) char *b="%c#define mittle(fp) char *b=%c%s%c;fprintf(fp, b, 10, 34, b, 34, 10);%c#define ahmed() main() {";fprintf(fp, b, 10, 34, b, 34, 10);
+#define ahmed() main() {FILE *fp=fopen("Grace_kid.c","w+");anfang(fp);mittle(fp);char *c="FILE *fp=fopen(%cGrace_kid.c%c,%cw+%c);anfang(fp);mittle(fp);char *c=%c%s%c;fprintf(fp, c, 34, 34, 34, 34, 34, c, 34, 10, 10);fclose(fp);}%cahmed()%c";fprintf(fp, c, 34, 34, 34, 34, 34, c, 34, 10, 10);fclose(fp);}
+ahmed()
