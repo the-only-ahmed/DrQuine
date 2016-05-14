a7e44fd76cdacfb0e9c60e6a8c9737be2af28f8e
Ahmed EL-KADHI <ael-kadh@e2r7p3.42.fr>
2016-05-14 15:53:27 +0200

    GCC flags + Grace py

--- Grace.c
+++ Grace.c
@@ -3,6 +3,6 @@
 	ahmed
 */
 #define anfang(fp) char *a="#include <stdio.h>%c/*%c%cahmed%c*/%c#define anfang(fp) char *a=%c%s%c;fprintf(fp, a, 10, 10, 9, 10, 10, 34, a, 34);";fprintf(fp, a, 10, 10, 9, 10, 10, 34, a, 34);
-#define mittle(fp) char *b="%c#define mittle(fp) char *b=%c%s%c;fprintf(fp, b, 10, 34, b, 34, 10);%c#define ahmed() main() {";fprintf(fp, b, 10, 34, b, 34, 10);
-#define ahmed() main() {FILE *fp=fopen("Grace_kid.c","w+");anfang(fp);mittle(fp);char *c="FILE *fp=fopen(%cGrace_kid.c%c,%cw+%c);anfang(fp);mittle(fp);char *c=%c%s%c;fprintf(fp, c, 34, 34, 34, 34, 34, c, 34, 10, 10);fclose(fp);}%cahmed()%c";fprintf(fp, c, 34, 34, 34, 34, 34, c, 34, 10, 10);fclose(fp);}
+#define mittle(fp) char *b="%c#define mittle(fp) char *b=%c%s%c;fprintf(fp, b, 10, 34, b, 34, 10);%c#define ahmed() int main() {";fprintf(fp, b, 10, 34, b, 34, 10);
+#define ahmed() int main() {FILE *fp=fopen("Grace_kid.c","w+");anfang(fp);mittle(fp);char *c="FILE *fp=fopen(%cGrace_kid.c%c,%cw+%c);anfang(fp);mittle(fp);char *c=%c%s%c;fprintf(fp, c, 34, 34, 34, 34, 34, c, 34, 10, 10);fclose(fp);return 0;}%cahmed()%c";fprintf(fp, c, 34, 34, 34, 34, 34, c, 34, 10, 10);fclose(fp);return 0;}
 ahmed()
---
+++ Grace.py
@@ -0,0 +1,5 @@
+#ahmed
+fp = open("Grace_kid.py", "w+")
+str = "#ahmed%cfp = open(%cGrace_kid.py%c, %cw+%c)%cstr = %c%s%c%cahmed = lambda fp : fp.write(str %c (10, 34, 34, 34, 34, 10, 34, str, 34, 10, 37, 10, 10))%cahmed(fp)%c"
+ahmed = lambda fp : fp.write(str % (10, 34, 34, 34, 34, 10, 34, str, 34, 10, 37, 10, 10))
+ahmed(fp)
