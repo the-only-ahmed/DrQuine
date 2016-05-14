af6c7a1eef40d84ebc6e4d7f68eae5bdb190ff80
ahmed <ael-kadh@student.42.fr>
2016-05-14 02:04:00 +0200

    col py

---
+++ Colleen.py
@@ -0,0 +1,7 @@
+def vide():
+	#ahmed
+	return None
+str = "def vide():%c%c#ahmed%c%creturn None%cstr = %c%s%c"
+str2 = "str2 = %c%s%c%cprint str %c (10, 9, 10, 9, 10, 34, str, 34)%cprint str2 %c (34, str2, 34, 10, 37, 10, 37)"
+print str % (10, 9, 10, 9, 10, 34, str, 34)
+print str2 % (34, str2, 34, 10, 37, 10, 37)
